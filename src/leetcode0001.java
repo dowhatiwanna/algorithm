4d8b63409f98822997eb2926b35d74b1b7a24bea
YCKJ3558 <fangbo@cloudwalk.com>
2020-09-17 18:00:54 +0800

    leetcode0001

---
+++ src/leetcode0001.java
@@ -0,0 +1,11 @@
+/**
+ * @author : YCKJ3558
+ * @since :2020/9/17 17:58
+ */
+public class leetcode0001 {
+    public int[] twoSum(int[] nums, int target) {
+        return new int[1];
+
+    }
+}
+
